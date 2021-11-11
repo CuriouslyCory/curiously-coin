edcaf8eb6971a816c8b3acd196e7d8a357f39d33
Cory Sougstad <cory@hau.me>
2021-11-11 16:54:00 -0700

    Added currency translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -25,6 +26,7 @@ import { HomeComponent } from './pages/home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatIconModule,
     MatToolbarModule,
     AppRoutingModule,
--- src/app/components/library/coin-sale/coin-sale.component.ts
+++ src/app/components/library/coin-sale/coin-sale.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoinSaleComponent implements OnInit {
 
+  fromVal: number = 0.0;
+  toVal: number = 0.0;
+  tokenValue: number = 0.5;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toChange(ev: any): void {
+    this.fromVal = this.toVal * this.tokenValue;
+  }
+
+  fromChange(ev: any): void {
+    this.toVal = this.fromVal / this.tokenValue;
   }
 
 }
