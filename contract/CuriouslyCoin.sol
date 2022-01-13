2411410c1c0ca893eec6740f099263e9af8b3b2e
Cory Sougstad <cory@hau.me>
2022-01-13 15:46:39 -0700

    Update to correct contract

--- contract/CuriouslyCoin.sol
+++ contract/CuriouslyCoin.sol
@@ -1,164 +1,65 @@
-// SPDX-License-Identifier: GPL-3.0
-
 /*
-    Contract base by: HashLips
-    Forked by: CuriouslyCory
-    Website: https://frognation.curiouslycory.com
-    Twitter: @FrogNationNFT
-    TG: t.me/FrogNationNFT
+Author: CuriouslyCory
+Website: https://curiouslycory.com
+Twitter: @CuriouslyCory
 */
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract FrogNationNFT is ERC721Enumerable, Ownable {
-  using Strings for uint256;
 
-  string public baseURI = "ipfs://Qmc8A19qUxy1VWeSDtJj9cGk1DAfE88E47Xb5BFn5Z6Hg1/";
-  string public baseExtension = ".json";
-  string public notRevealedUri = "https://frognationnft.curiouslycory.com/images/not-revealed.png";
-  uint256 public cost = 15 ether;
-  uint256 public maxSupply = 8888;
-  uint256 public maxMintAmount = 10;
-  uint256 public nftPerAddressLimit = 10;
-  bool public paused = false;
-  bool public revealed = true;
-  bool public onlyWhitelisted = true;
-  address[] public whitelistedAddresses;
-  mapping(address => uint256) public addressMintedBalance;
-
-  constructor() ERC721("FrogNationNFT", "FNNFT") {
-  }
-
-  // internal
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
-  }
-
-  // public
-  function mint(uint256 _mintAmount) public payable {
-    require(!paused, "the contract is paused");
-    uint256 supply = totalSupply();
-    require(_mintAmount > 0, "need to mint at least 1 NFT");
-    require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
-    require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
-
-    if (msg.sender != owner()) {
-        if(onlyWhitelisted == true) {
-            require(isWhitelisted(msg.sender), "user is not whitelisted");
-            uint256 ownerMintedCount = addressMintedBalance[msg.sender];
-            require(ownerMintedCount + _mintAmount <= nftPerAddressLimit, "max NFT per address exceeded");
-        }
-        require(msg.value >= cost * _mintAmount, "insufficient funds");
-    }
-
-    for (uint256 i = 1; i <= _mintAmount; i++) {
-      addressMintedBalance[msg.sender]++;
-      _safeMint(msg.sender, supply + i);
-    }
-  }
-  
-  function isWhitelisted(address _user) public view returns (bool) {
-    for (uint i = 0; i < whitelistedAddresses.length; i++) {
-      if (whitelistedAddresses[i] == _user) {
-          return true;
-      }
-    }
-    return false;
-  }
-
-  function walletOfOwner(address _owner)
-    public
-    view
-    returns (uint256[] memory)
-  {
-    uint256 ownerTokenCount = balanceOf(_owner);
-    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-    for (uint256 i; i < ownerTokenCount; i++) {
-      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
-    }
-    return tokenIds;
-  }
-
-  function tokenURI(uint256 tokenId)
-    public
-    view
-    virtual
-    override
-    returns (string memory)
-  {
-    require(
-      _exists(tokenId),
-      "ERC721Metadata: URI query for nonexistent token"
-    );
+contract CuriouslyCoin is ERC20, ERC20Burnable, Ownable {
+    uint256 public _maxSupply = 250000000 * 10 ** decimals();
+    uint256 public _cost = 0.05 ether; //cost of 0.05 bnb
+    string private constant _name = unicode"Curiously Coin";
+    string private constant _symbol = unicode"CCN";
     
-    if(revealed == false) {
-        return notRevealedUri;
+    constructor() ERC20(_name, _symbol) {
+        // send the full supply to the contract.
+        _mint(address(this), _maxSupply);
     }
 
-    string memory currentBaseURI = _baseURI();
-    return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
-        : "";
-  }
-
-  //only owner
-  function reveal() public onlyOwner {
-      revealed = true;
-  }
-  
-  function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
-    nftPerAddressLimit = _limit;
-  }
-  
-  function setCost(uint256 _newCost) public onlyOwner {
-    cost = _newCost;
-  }
-
-  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
-    maxMintAmount = _newmaxMintAmount;
-  }
-
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
-  }
-
-  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
-    baseExtension = _newBaseExtension;
-  }
-  
-  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-    notRevealedUri = _notRevealedURI;
-  }
-
-  function pause(bool _state) public onlyOwner {
-    paused = _state;
-  }
-  
-  function setOnlyWhitelisted(bool _state) public onlyOwner {
-    onlyWhitelisted = _state;
-  }
-  
-  function whitelistUsers(address[] calldata _users) public onlyOwner {
-    delete whitelistedAddresses;
-    whitelistedAddresses = _users;
-  }
- 
-  function withdraw() public payable onlyOwner {
-    // This will pay HashLips 5% of the initial sale.
-    // You can remove this if you want, or keep it in to support HashLips and his channel.
-    // =============================================================================
-    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-    require(hs);
-    // =============================================================================
+    // local currency balance (ETH, BNB, SOL)
+    function thisBalance() public view returns (uint) {
+        return balanceOf(address(this));
+    }
     
-    // This will payout the owner 95% of the contract balance.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
-    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-    require(os);
-    // =============================================================================
-  }
+    // allow owner to withdraw any on chain currency from contract 
+    function withdrawBalance() public payable onlyOwner {
+        require(payable(msg.sender).send(address(this).balance));
+    }
+    
+    // allow owner to withdraw tokens from contract
+    function withdrawTokens(uint256 _amount) public  onlyOwner {
+        require(thisBalance() > _amount, "Not Enough Tokens in Supply");
+        _transfer(address(this), msg.sender, _amount);
+    }
+    
+    // allow owner to withdraw foreign erc20 tokens from contract
+    function withdrawERC20Tokens(address _contractAddress, uint256 _amount) public onlyOwner {
+        IERC20(_contractAddress).transfer(msg.sender, _amount);
+    }
+    
+    // purchase tokens directly from contract for presale
+    function purchaseTokens(uint256 _amount) public payable {
+        require(thisBalance() > _amount, "Not Enough Tokens in Supply");
+        require(msg.value >= _cost * (_amount / 10 ** decimals()), "Not enough value for amount of tokens.");
+        _transfer(address(this), msg.sender, _amount);
+    }
+    
+    // update cost of presale tokens
+    function setCost(uint256 _newCost) public onlyOwner {
+        _cost = _newCost;
+    }
+    
+    // read current cost (it's already public I don't need this)
+    function getCost() public view returns(uint256) {
+        return _cost;
+    }
+
 }
